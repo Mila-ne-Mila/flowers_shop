0143583136261610b3a84aac5d85ee0e36dd463e
Mila-ne-Mila <nadkernichnaia.ln@gmail.com>
2022-11-29 16:09:13 +0200

    add http reques

--- app.js
+++ app.js
@@ -1,90 +1,51 @@
-const flowers = [{
-    image: "https://i.ibb.co/qk3WZzd/bouquet-cart1.png",
-    name: "Ніжна свіжість",
-    flovers: "троянди та лілії",
-    price: 445
-  },
-  {
-    image: "https://i.ibb.co/kSqMtYg/bouquet-cart2.png",
-    name: "Сорбет",
-    flovers: "хризантеми та троянди",
-    price: 450
-  },
-
-  {
-    image: "https://i.ibb.co/P9mTcQT/bouquet-cart3.png",
-    name: "Yellow song",
-    flovers: "соняшники та солідаго",
-    price: 455
-  },
-  {
-    "image": "https://i.ibb.co/M8NL0gT/bouquet-cart4.png",
-    "name": "Персиковий нектар",
-    "flovers": "лілії та троянди",
-    "price": 455
-  },
-  {
-    "image": "https://i.ibb.co/LYMJ9mc/bouquet-cart5.png",
-    "name": "Аврора",
-    "flovers": "лілії та троянди",
-    "price": 460
-  },
-  {
-    "image": "https://i.ibb.co/Y7jdbq5/bouquet-cart6.png",
-    "name": "Класика кохання",
-    "flovers": "червоні троянди",
-    "price": 465
-  }
-]
-
-
-//   <div class="flowers_card">
-//   <img class="frower_card_img" src="bouquet-cart1.png" alt="" />   
-//   <div class="flowers_name">Ніжна свіжість: троянди та лілії</div>
-//   <div class="flower_price"> 445грн</div>
-// </div>
-
 
 
   function createContent(flower) {
-        const div = document.createElement('div');
-        div.classList.add('flowers_card');
 
-        const img = document.createElement('img');
-        img.classList.add('frower_card_img');
-        img.style.backgroundImage = `url(${flower.image})`;
+            const div = document.createElement('div');
+            div.classList.add('flowers_card');
 
-        const divFlowersName = document.createElement('div');
-        divFlowersName.classList.add('flowers_name');
-        divFlowersName.textContent = `${flower.name} : ${flower.flovers}`;
+            const img = document.createElement('img');
+            img.classList.add('frower_card_img');
 
-        const divFlowersPrice = document.createElement('div');
-        divFlowersPrice.classList.add('flower_price');
-        divFlowersPrice.textContent = flower.price;
+            img.style.backgroundImage = `url(${flower.image})`;
+            const divFlowersName = document.createElement('div');
 
-        div.appendChild(img);
-        div.appendChild(divFlowersName);
-        div.appendChild(divFlowersPrice);
+            divFlowersName.classList.add('flowers_name');
+            divFlowersName.textContent = `${flower.name} : ${flower.flovers}`;
 
-        return div;
+            const divFlowersPrice = document.createElement('div');
+            divFlowersPrice.classList.add('flower_price');
+            divFlowersPrice.textContent = flower.price;
+
+            div.appendChild(img);
+            div.appendChild(divFlowersName);
+            div.appendChild(divFlowersPrice);
+            return div;
   }
+      function appendContent(content) {
+              const el = document.getElementById('content')
+              el.appendChild(content);
+  }
+      function init() {
+              fetch('https://run.mocky.io/v3/275cc213-7485-4396-b1ba-c1969194d67f')
+              .then((resp) => resp.json())
+              .then((data) => {
+              const fragment = document.createDocumentFragment();
 
-function appendContent(content) {
-    const el = document.getElementById('content')
-    el.appendChild(content);
+              data.forEach((flower) => {
+              fragment.appendChild(createContent(flower));
+  })
+
+  appendContent(fragment);
+              
+  });
 }
 
+  init();
 
-  function init(flowers) {
-    const fragment = document.createDocumentFragment();
-    flowers.forEach((flower) => {
-        fragment.appendChild(createContent(flower));
-    })
 
-    appendContent(fragment);
-  }
 
-  init(flowers);
 
 
 
@@ -114,7 +75,4 @@ function appendContent(content) {
       
     }
     xhr.send();
-  
-
-
   
